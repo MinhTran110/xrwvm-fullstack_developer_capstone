bebd220e8e656bc7b11920888f79174e6124eaad
Minh110 <tranqm2006@gmail.com>
2026-06-10 00:37:25 +0700

    Add Register component

---
+++ server/frontend/src/components/Register/Register.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+function Register() {
+  const [userName, setUserName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <input
+        type="text"
+        placeholder="Username"
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Register</button>
+    </div>
+  );
+}
+
+export default Register;
